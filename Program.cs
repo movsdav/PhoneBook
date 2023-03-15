45c2d60908c2173606dc87bad548d911b617cad2
David <movsis.david@gmail.com>
2023-03-15 16:00:51 +0400

    + File read + String Validation

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PhoneBook;
+
+public class Program
+{
+    private static User[] userStorage;
+
+    public static void Main(string[] args)
+    {
+        ShowUsers();
+        ShowValidationMessages();
+    }
+
+    private static string GetPath()
+    {
+        Console.Write("Enter the path: ");
+        string path = Console.ReadLine(); // TODO: handle invalid path exception!
+
+        Console.WriteLine();
+
+        return path;
+    }
+    private static string[] GetLinesFromFile(string path)
+    {
+        string[] lines;
+        using (StreamReader sr = new StreamReader(path))
+            lines = sr.ReadToEnd().Trim().Split("\n");
+
+        return lines;
+    }
+    private static User[] GetUsersFromLines(string[] lines)
+    {
+        List<User> result = new List<User>();
+        for (int i = 0; i < lines.Length; i++)
+            result.Add(User.CreateUserFromLine(lines[i], i + 1));
+
+        return result.ToArray<User>();
+    }
+
+    private static void ShowUsers()
+    {
+        string path = GetPath();
+
+        string[] lines = GetLinesFromFile(path);
+
+        userStorage = GetUsersFromLines(lines);
+
+        Console.WriteLine("File structure:");
+        foreach (User user in userStorage)
+            Console.WriteLine(user);
+
+        Console.WriteLine();
+    }
+    private static void ShowValidationMessages()
+    {
+        Console.WriteLine("\nValidation messages:");
+
+        string? errorMessage;
+        foreach (User user in userStorage)
+        {
+            errorMessage = user.GetErrorMessage();
+            if (errorMessage != null)
+                Console.WriteLine(errorMessage);
+        }
+    }
+}
---
+++ classes/User.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace PhoneBook;
+
+public class User
+{
+    private static string[] validSeparators = { ":", "-" };
+
+    public static User CreateUserFromLine(string line, int lineIndex)
+    {
+        string[] splitedLine = line.Split(" ");
+
+        string separator = splitedLine[^2];
+        string phoneNumberCode = splitedLine[^1];
+        string name = splitedLine[0];
+        string surname = splitedLine[1] == separator ? "" : splitedLine[1];
+
+        User user = new User(name, surname, phoneNumberCode, line)
+        {
+            Id = lineIndex,
+            Separator = separator
+        };
+
+        user.Validate();
+        return user;
+    }
+    public override string ToString()
+    {
+        if (initialUserLine != null)
+            return initialUserLine;
+
+        return $"{Name} {Surname} {Separator} {PhoneNumberCode}";
+    }
+
+    private string? initialUserLine;
+    private string? errorMessage;
+
+    public int Id { get; init; }
+    public string Separator { get; init; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PhoneNumberCode { get; set; }
+
+    public User(string name, string surname, string phoneNumberCode, string? initalLine = null)
+    {
+        initialUserLine = initalLine;
+        Name = name;
+        Surname = surname;
+        PhoneNumberCode = phoneNumberCode;
+    }
+
+    private void Validate()
+    {
+        StringBuilder result = new StringBuilder("");
+
+        if (!validSeparators.Contains(Separator))
+            result.Append("separator should be ':' or '-'.");
+
+        if (PhoneNumberCode.Length - 1 != 9)
+        {
+            result.Replace(".", ",");
+            result.Append("phone number should be 9 digits.");
+        }
+
+        if (result.ToString() == "")
+        {
+            errorMessage = null;
+        }
+        else
+        {
+            result.Insert(0, $"Line {Id}: ");
+            errorMessage = result.ToString();
+        }
+    }
+
+    public string? GetErrorMessage() => errorMessage;
+}
